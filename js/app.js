a3c19bfe1ba57a79872b2a850ae8ca4226155cb3
Łukasz Wieczorek <lukasz.wieczorek@unit9.com>
2015-02-04 16:36:58 +0100

    Working app

--- js/app.js
+++ js/app.js
@@ -1,23 +1,42 @@
-// TODO keyboard controls
+var ANIMATION_TIME = 256;
+var PLAYING_TIME = 2048;
+var LOWEST_FREQUENCY = 110;
+var HIGHEST_FREQUENCY = 880;
+var SPAN_DIVIDER = 10;
 
 var firstFrequency, secondFrequency;
 var currentPlayer;
+var isRoundStarting;
+var $first, $second, $lower, $equal, $higher;
 
 var Player = function() {
     this.pause = function() {}
 };
 var player = new Player();
 
+function getDifference(frequency) {
+    var base = LOWEST_FREQUENCY;
+    while(true) {
+        if (base <= frequency && frequency < 2 * base) {
+            break;
+        } else {
+            base *= 2;
+        }
+    }
+    return base / SPAN_DIVIDER;
+}
+
 function generateTones() {
-    firstFrequency = rand(55, 880);
-    // TODO use cents as difference
-    var difference = rand(-20, 20);
+    firstFrequency = rand(LOWEST_FREQUENCY, HIGHEST_FREQUENCY);
+    var d = getDifference(firstFrequency);
+    var difference = rand(-d, d);
     secondFrequency = firstFrequency + difference;
+    currentPlayer = 'none';
 }
 
 function rand(minimum, maximum) {
     var rand = minimum + Math.random() * (maximum - minimum + 1);
-    return Math.floor(rand)
+    return Math.floor(rand);
 }
 
 function setPlayer(frequency, _currentPlayer) {
@@ -42,10 +61,10 @@ function checkAnswer(answer) {
     var $advices = $('.advice');
     if (success) {
         $body.removeClass('body-inverted');
-        $advices.removeClass('advice-inverted')
+        $advices.removeClass('advice-inverted');
     } else {
         $body.addClass('body-inverted');
-        $advices.addClass('advice-inverted')
+        $advices.addClass('advice-inverted');
     }
     player.pause();
     generateTones();
@@ -58,52 +77,98 @@ function animate(self) {
         'height': '-=16px',
         'margin-top': '+=8px',
         'margin-left': '+=8px'
-    }, 256, 'swing', function() {
+    }, ANIMATION_TIME, 'swing', function() {
         $this.animate({
             'width': '+=16px',
             'height': '+=16px',
             'margin-top': '-=8px',
             'margin-left': '-=8px'
-        }, 256)
+        }, ANIMATION_TIME);
     });
 }
 
-// TODO setTimeout / setInterval on new pair
-$(document).ready(function() {
+function play(frequency, name) {
+    if (currentPlayer != name) {
+        player.pause();
+        setPlayer(frequency, name);
+        player.play();
+    } else {
+        player.pause();
+        currentPlayer = 'none';
+    }
+}
+
+function startRound() {
+    isRoundStarting = true;
     generateTones();
-    currentPlayer = 'none';
-    $('#first').click(function() {
-        animate(this);
-        if (currentPlayer != 'first') {
+    play(firstFrequency, 'first');
+    setTimeout(function() {
+        play(secondFrequency, 'second');
+        setTimeout(function() {
             player.pause();
-            setPlayer(firstFrequency, 'first');
-            player.play()
-        } else {
-            player.pause();
-            currentPlayer = 'none';
-        }
+            isRoundStarting = false;
+        }, PLAYING_TIME);
+    }, PLAYING_TIME);
+}
+
+function check(self, frequency, name) {
+    if (!isRoundStarting) {
+        animate(self);
+        play(frequency, name);
+    }
+}
+
+function answer(self, name) {
+    if (!isRoundStarting) {
+        animate(self);
+        checkAnswer(name);
+        startRound();
+    }
+}
+
+function setupElements() {
+    $first = $('#first');
+    $second = $('#second');
+    $lower = $('#lower');
+    $equal = $('#equal');
+    $higher = $('#higher');
+}
+
+function setupKeyBindings() {
+    Mousetrap.bind('left', function() {
+        $first.click();
     });
-    $('#second').click(function() {
-        animate(this);
-        if (currentPlayer != 'second') {
-            player.pause();
-            setPlayer(secondFrequency, 'second');
-            player.play()
-        } else {
-            player.pause();
-            currentPlayer = 'none';
-        }
+    Mousetrap.bind('right', function() {
+        $second.click();
     });
-    $('#lower').click(function() {
-        animate(this);
-        checkAnswer('lower')
+    Mousetrap.bind('down', function() {
+        $lower.click();
     });
-    $('#equal').click(function() {
-        animate(this);
-        checkAnswer('equal')
+    Mousetrap.bind('space', function() {
+        $equal.click();
     });
-    $('#higher').click(function() {
-        animate(this);
-        checkAnswer('higher')
+    Mousetrap.bind('up', function() {
+        $higher.click();
     })
+}
+
+$(document).ready(function() {
+    setupElements();
+    setupKeyBindings();
+    $first.click(function() {
+        check(this, firstFrequency, 'first');
+    });
+    $second.click(function() {
+        check(this, secondFrequency, 'second');
+    });
+    $lower.click(function() {
+        answer(this, 'lower');
+    });
+    $equal.click(function() {
+        answer(this, 'equal');
+    });
+    $higher.click(function() {
+        answer(this, 'higher');
+    });
+    startRound();
 });
